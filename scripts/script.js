120413d06b52cfbf13b05f2f46b3f92bb5bf49b8
StrikerJuan <parkavenue.jcha@gmail.com>
2024-04-30 01:02:52 -0600

    Section 8 y 10

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("No se encontró el elemento de audio.");
     }
 
+    
     //  Creacion de cronometro
+    var targetDate = new Date("2024-07-20T17:30:00");
+    var currentDate = new Date();
     function crearCountdown(){
-        var targetDate = new Date("2024-07-20T17:30:00");
-        var currentDate = new Date();
+        
         var diferencia = targetDate - currentDate;
 
         var days = Math.floor(diferencia / (1000 * 60 * 60 * 24));
@@ -19,10 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
         var minu = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
         var seco = Math.floor((diferencia % (1000 * 60)) / 1000);
 
+
         document.getElementById("dia").innerText = days;
         document.getElementById("hor").innerText = hour;
         document.getElementById("min").innerText = minu;
         document.getElementById("seg").innerText = seco;
+        
     }
 
     setInterval(crearCountdown, 1000);
@@ -52,4 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     muestraLi();
+
+    var anio = currentDate.getFullYear();
+    document.getElementById("anio").innerText = anio;
 });
